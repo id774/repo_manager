23bb93ff4ac1305ba9b40c3f81ef9da743f03a99
id774 <idnanashi@gmail.com>
2012-07-10 16:28:26 +0900

    19 Clean Up

--- spec/stub/git_delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-test -n "$1" || exit 1
-test -n "$2" && exit 1
-TODAY=`date '+%F'`
-test -d /home/trash/git/$TODAY || mkdir /home/trash/git/$TODAY
-chmod 770 /home/trash/git/$TODAY
-test -d /home/trash/git/$TODAY/$1.git && rm -rf /home/trash/git/$TODAY/$1.git
-test -d /var/lib/git/$1.git && mv -f /var/lib/git/$1.git /home/trash/git/$TODAY/
--- spec/stub/svn_create.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-test -n "$1" || exit 1
-test -n "$2" && exit 1
-svnadmin create ./tmp/$1
-chmod -R o-rwx,g+w ./tmp/$1
